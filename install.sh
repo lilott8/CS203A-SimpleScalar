0dce0f288c0efe621aafae190428f110ea99be97
lilott8 <lilott8@gmail.com>
2013-12-04 21:06:17 -0800

    name changes

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 make
 make config-alpha
 make sim-outorder
-./sim-outorder ./benchmarks/anagram.alpha -fetch:ifqsize 1 -commit:width 1 -decoder:width 1 -issue:width 1 
+./sim-outorder ./benchmarks/anagram.alpha -fetch:ifqsize 1 -commit:width 4 -decoder:width 2 -issue:width 1 
